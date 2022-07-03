7c81b87f77cd876c5f9af5f62ab78196f46af095
Hubert13888 <hubertsiwczynski@gmail.com>
2022-07-03 20:39:55 +0200

    test2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
+  const [counter, setCouter] = useState(0)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <button onClick={() => setCouter(counter + 1)}>Kliknij</button>
+        <p>{counter}</p>
+        {counter%5===0 ?<p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> : <></>}
+        
         <a
           className="App-link"
           href="https://reactjs.org"
